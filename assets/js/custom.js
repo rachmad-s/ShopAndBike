653d7e303b606029325dc659a1dfe4ccd3c80a77
Rahmat Syaefullah <31819246+rahmatsya@users.noreply.github.com>
2021-05-19 13:49:31 +0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+
+
+const heroSwiper = new Swiper('.hero-carousel-container', {
+    slidesPerView: 4,
+    spaceBetween: 22, 
+    loop: 'true',
+    // navigation: {
+    //     nextEl: '.swiper-button-prev',
+    //     prevEl: '.swiper-button-next'
+    // }
+});
