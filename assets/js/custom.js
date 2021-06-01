4845e421344fa0903fc3a7e6b31bfdef61a01635
rahmatsya <rachmadsyaefullah13@gmail.com>
2021-06-01 17:20:37 +0700

    add carousel into hero section

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    $('.hero-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ['<object data="assets/icons/chevron-left.svg">', '<object data="assets/icons/chevron-right.svg">'], 
+        dots: false
+    })
+    
+})
+
+
+feather.replace();
